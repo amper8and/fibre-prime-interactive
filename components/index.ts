9b1c357f43063ffa36aac0be3948d623685fafa6
amper8and <amper8and@users.noreply.github.com>
2026-03-06 19:46:19 +0000

    Add component index for centralized exports

---
+++ components/index.ts
@@ -0,0 +1,35 @@
+/**
+ * Component Index
+ * Central export point for all components
+ */
+
+// Layout Components
+export { AppShell } from './layout/AppShell';
+export { TopNav } from './layout/TopNav';
+export { PageContainer } from './layout/PageContainer';
+export { SectionHeader } from './layout/SectionHeader';
+
+// Home Experience Components
+export { RoomNavigator } from './home/RoomNavigator';
+export { RoomScene } from './home/RoomScene';
+export { DeviceHotspot } from './home/DeviceHotspot';
+
+// Device Components
+export { DevicePanel } from './device/DevicePanel';
+
+// Bundle Components
+export { BundleWidget } from './bundle/BundleWidget';
+export { BundleDrawer } from './bundle/BundleDrawer';
+export { BundleLineItem } from './bundle/BundleLineItem';
+export { FibrePlanSelector } from './bundle/FibrePlanSelector';
+
+// Marketplace Components
+export { ProductCard } from './marketplace/ProductCard';
+export { ProductGrid } from './marketplace/ProductGrid';
+export { CatalogFilterBar } from './marketplace/CatalogFilterBar';
+
+// Utility Components
+export { ToastNotification } from './utility/ToastNotification';
+export { LoadingState } from './utility/LoadingState';
+export { EmptyState } from './utility/EmptyState';
+export { StatusIndicator } from './utility/StatusIndicator';
